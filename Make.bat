e30179353475daad9f0ec674a06bd44b811625a3
TheDeadFish <michaelstamper1@gmail.com>
2021-01-29 22:08:18 +0000

    updated Make.bat for new build enviroment

--- Make.bat
+++ Make.bat
@@ -1,12 +1,4 @@
-@setlocal
-@call egcc.bat
-@if not exist build32 mkdir build32
-@set CFLAGS=%CFLAGS2%
-@set CXXFLAGS=%CCFLAGS2%
-@pushd build32
-@%CMAKE%  ..\.
-@mingw32-make
-@popd
+@call cmake_gcc
 
 :: install library
 @copy /Y src\pdblib.h %PROGRAMS%\local\include
