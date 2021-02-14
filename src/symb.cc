8dd1a34c6247688eaccf39251e33bfffbfb2d92f
TheDeadFish <michaelstamper1@gmail.com>
2021-02-14 22:28:49 +0000

    symb.cc: support S_PROCREF_ST, S_LPROCREF_ST; string not in reclen

--- src/symb.cc
+++ src/symb.cc
@@ -5,9 +5,11 @@
 bool pdb_symb_parse(PdbFile& pdb, xarray<byte> file)
 {
 	auto pos = xRngPtr<byte>(file.data, file.len);
-	while(pos.chk()) {
+	while(1) {
 	
 		// validate the record
+		pos.data += (file.data-pos.data)&3;
+		if(!pos.chk()) break;
 		SYMTYPE* rec = Void(pos.data);
 		int len = rec->reclen+2;
 		if((len < 4)||(pos.count() < len))
@@ -26,6 +28,15 @@ bool pdb_symb_parse(PdbFile& pdb, xarray<byte> file)
 		}
 		
 		pos.data += len;
+		
+		// strings not in reclen
+		if((rec->rectyp == S_PROCREF_ST)
+		||(rec->rectyp == S_LPROCREF_ST)) {
+			if(!pos.chk()) return false;
+			len = *pos.data++;
+			if(pos.count() < len) return false;
+			pos.data += len;
+		}
 	}
 
 	return true;
