e409acac95b9e77e356f029e262c473f7f4d47fd
TheDeadFish <michaelstamper1@gmail.com>
2021-05-29 16:12:56 +0000

    dbi: made error codes positive, (why were they negative?)

--- src/dbi.cc
+++ src/dbi.cc
@@ -15,37 +15,37 @@ int PdbFile::_dbi_init()
 	dump_dbiHdr(dataPos._ptr());
 #endif
 	if(!dataPos.pGet<DbiHdr>(dbiHdr))
-		return -1;
+		return 1;
 
 	// Module Info
 	if(!dataPos._chkSetEnd(dbiHdr->ModInfoSize))
-		return -2;
+		return 2;
 	while(dataPos.chk()) {
 #ifdef DEBUG_DUMP
 		dump_modInfo(dataPos._ptr());
 #endif
 		auto& mi = modInfo.xnxalloc();
-		if(!dataPos.pGet(mi.hdr)) return -2;
-		if(!dataPos.getStr()) return -2;
-		if(!dataPos.pGetStr(mi.ObjFileName)) return -2;
+		if(!dataPos.pGet(mi.hdr)) return 2;
+		if(!dataPos.getStr()) return 2;
+		if(!dataPos.pGetStr(mi.ObjFileName)) return 2;
 		dataPos.align(4);
 	}
 
 	// Section Contribution
 	dataPos.setEnd(msf[3]);
 	if(!dataPos.pGetXa(sectCont, dbiHdr->SectionContributionSize))
-		return -3;
+		return 3;
 
 	// skip unsuported
-	if(!dataPos.get(dbiHdr->SectionMapSize)) return -4;
-	if(!dataPos.get(dbiHdr->SourceInfoSize))  return -4;
-	if(!dataPos.get(dbiHdr->TypeServerMapSize))  return -4;
-	if(!dataPos.get(dbiHdr->ECSubstreamSize)) return -4;
+	if(!dataPos.get(dbiHdr->SectionMapSize)) return 4;
+	if(!dataPos.get(dbiHdr->SourceInfoSize))  return 4;
+	if(!dataPos.get(dbiHdr->TypeServerMapSize))  return 4;
+	if(!dataPos.get(dbiHdr->ECSubstreamSize)) return 4;
 
 	// optional Debug Header
 	DbiOptHdr* _optHdr;
 	if(!dataPos._pGet(_optHdr, dbiHdr->OptionalDbgHeaderSize))
-		return -5;
+		return 5;
 	memncpy(&dbiOptHdr, _optHdr, sizeof(DbiOptHdr),
 		dbiHdr->OptionalDbgHeaderSize, -1);
 
