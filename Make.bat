4d08b7c7495c7509e2d778d5cf7111be0005c952
TheDeadFish <michaelstamper1@gmail.com>
2021-05-29 15:40:42 +0000

    make.bat: copy incldue files to local\include\pdblib

--- Make.bat
+++ Make.bat
@@ -1,5 +1,9 @@
-@call cmake_gcc
+@call cmake_gcc DEBUG
 
 :: install library
-@copy /Y src\pdblib.h %PROGRAMS%\local\include
+set DIR=%PROGRAMS%\local\include\pdblib
+@mkdir %DIR%
+@copy /Y src\pdblib.h %DIR%
+@copy /Y src\msf.h %DIR%
+@copy /Y include\cvinfo.h %DIR%
 @copy /Y bin\libpdbLib.a %PROGRAMS%\local\lib32
